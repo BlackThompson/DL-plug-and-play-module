858505f6206ac2d3b443e7964e576641e7f7e08a
Black <1587754061@qq.com>
2023-10-28 21:41:07 +0800

    update

---
+++ swiftFormer.py
@@ -0,0 +1,57 @@
+"""
+SwiftFormer
+"""
+import torch
+import torch.nn as nn
+import einops
+
+
+class EfficientAdditiveAttnetion(nn.Module):
+    """
+    Efficient Additive Attention module for SwiftFormer.
+    Input: tensor in shape [B, N, D]
+    Output: tensor in shape [B, N, D]
+    """
+
+    def __init__(self, in_dims=512, token_dim=256, num_heads=2):
+        super().__init__()
+
+        self.to_query = nn.Linear(in_dims, token_dim * num_heads)
+        self.to_key = nn.Linear(in_dims, token_dim * num_heads)
+
+        self.w_g = nn.Parameter(torch.randn(token_dim * num_heads, 1))
+        self.scale_factor = token_dim ** -0.5
+        self.Proj = nn.Linear(token_dim * num_heads, token_dim * num_heads)
+        self.final = nn.Linear(token_dim * num_heads, token_dim)
+
+    def forward(self, x):
+        query = self.to_query(x)
+        key = self.to_key(x)
+
+        query = torch.nn.functional.normalize(query, dim=-1)  # BxNxD
+        key = torch.nn.functional.normalize(key, dim=-1)  # BxNxD
+
+        query_weight = query @ self.w_g  # BxNx1 (BxNxD @ Dx1)
+        A = query_weight * self.scale_factor  # BxNx1
+
+        A = torch.nn.functional.normalize(A, dim=1)  # BxNx1
+
+        G = torch.sum(A * query, dim=1)  # BxD
+
+        G = einops.repeat(
+            G, "b d -> b repeat d", repeat=key.shape[1]
+        )  # BxNxD
+
+        out = self.Proj(G * key) + query  # BxNxD
+
+        out = self.final(out)  # BxNxD
+
+        return out
+
+
+# 输入 B N C ,  输出 B N C
+if __name__ == '__main__':
+    block = EfficientAdditiveAttnetion(64, 32).cuda()
+    input = torch.rand(3, 64 * 64, 64).cuda()
+    output = block(input)
+    print(input.size(), output.size())
